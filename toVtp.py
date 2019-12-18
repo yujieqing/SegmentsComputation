c1d1979eb6ca5004dfc02a41992f52a4719c3f75
yujieqing <yujieqing@gmail.com>
2019-12-18 09:51:47 -0700

    Revert "Update toVtp.py"
    This reverts commit 6327ff3e0430057da8073c93335456f9be80d49d.

--- toVtp.py
+++ toVtp.py
@@ -3,10 +3,10 @@ import numpy as np
 import os
 print("convert to .vtp format")
 rltPath=os.path.split(os.path.realpath(__file__))[0]
-insegment0=rltPath+"\\output\\tradition_centSize1.txt"
-insegment1=rltPath+"\\output\\raytracing_centSize1.txt"
-outsegment0=rltPath+"\\output\\tradition—a.vtp"
-outsegment1=rltPath+"\\output\\raytracing—a.vtp"
+insegment0=rltPath+"\\output\\tradition_centSize0.txt"
+insegment1=rltPath+"\\output\\raytracing_centSize0.txt"
+outsegment0=rltPath+"\\output\\tradition.vtp"
+outsegment1=rltPath+"\\output\\raytracing.vtp"
 
 data0 = np.loadtxt(open(insegment0, "rb"), delimiter=",", skiprows=0, ndmin=2)
 data1 = np.loadtxt(open(insegment1, "rb"), delimiter=",", skiprows=0, ndmin=2)
@@ -14,12 +14,12 @@ vtkXMLFile.vtkXMLVoxel.to_vtp(data0,outsegment0,0,"segment")
 vtkXMLFile.vtkXMLVoxel.to_vtp(data1,outsegment1,0,"segment")
 
 voxcntFile=rltPath+"\\input\\voxelModel-CentSize.txt"
-outvoxcntFile=rltPath+"\\output\\voxelModel—a.vtp"
+outvoxcntFile=rltPath+"\\output\\voxelModel.vtp"
 vxlmdl = np.loadtxt(open(voxcntFile, "rb"), delimiter=",", skiprows=0, ndmin=2)
 vtkXMLFile.vtkXMLVoxel.to_vtp(vxlmdl,outvoxcntFile,5,"voxelmodel")
 
 coordFile=rltPath+"\\input\\rayCoordinates10.txt"
-rayOutFile=rltPath+"\\output\\ray—a.vtp"
+rayOutFile=rltPath+"\\output\\ray.vtp"
 newdata=[]
 raydata = np.loadtxt(open(coordFile, "rb"), delimiter=",", skiprows=0, ndmin=2)
 for i in range(0,len(raydata)):
